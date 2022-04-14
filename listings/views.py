4da1aca337f62a036135c3745d6d67b463eab132
yusuf963 <mohammad963yusuf@gmail.com>
2022-04-14 21:36:59 +0100

    create listings & realtors apps and setup listing template

---
+++ listings/views.py
@@ -0,0 +1,12 @@
+from django import views
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'templates/listings/listings.html')
+
+def listing(request):
+    return render(request,'templates/listings/listing.html')
+
+def search(request):
+    return render(request, 'templates/listings/search.html')
