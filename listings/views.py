9016015b753890e2619ca284e0697636a8a42890
yusuf963 <mohammad963yusuf@gmail.com>
2022-04-15 23:04:45 +0100

    add filter logic to listing/views file and populate html search templtate

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
 from webbrowser import get
 from django.shortcuts import (render, get_object_or_404)
 from django.core.paginator import (Paginator, EmptyPage, PageNotAnInteger)
+from .choices import (bedroom_choices, price_choices, state_choices)
+
 from .models import Listing
 
 
@@ -25,4 +27,42 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'templates/listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+     # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']  
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)   
+    # City 
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city) # iexact means case insensitive
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms) # lte means less than or equal to
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price) # lte means less than or equal to
+    # bathrooms
+    if 'bathrooms' in request.GET:
+        bathrooms = request.GET['bathrooms']
+        if bathrooms:
+            queryset_list = queryset_list.filter(bathrooms__lte=bathrooms)
+    context = {
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'state_choices': state_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+    return render(request, 'templates/listings/search.html', context)
