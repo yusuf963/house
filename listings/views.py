e857b58ae12b834f77d0a1c06e84299cc803cc2e
yusuf963 <mohammad963yusuf@gmail.com>
2022-04-16 13:27:30 +0100

    handle user register post request, handle user login request, handle user logout request, set error/sucess meg to UI using jquery, and edit navbar to show user name

--- listings/views.py
+++ listings/views.py
@@ -8,7 +8,7 @@ from .models import Listing
 
 def index(request):
     listings = Listing.objects.order_by(
-        '-list_date').filter(is_published=True)[:3]
+        '-list_date').filter(is_published=True)
     paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
