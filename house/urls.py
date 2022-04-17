001638d31e2c264bac19d05d001d71960f95503b
yusuf963 <mohammad963yusuf@gmail.com>
2022-04-17 12:35:14 +0100

    install gunicorn to serve app on remote server and create Procfile

--- house/urls.py
+++ house/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import (path, include)
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -25,3 +26,4 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
