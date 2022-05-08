af317048792eeb9f1c0c0340cef6b41801c1d8cd
yusuf963 <mohammad963yusuf@gmail.com>
2022-05-08 21:34:44 +0100

    remove connect code and fix env variable

--- house/settings.py
+++ house/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from ctypes import cast
 from django.contrib.messages import constants as messages
 import os
 import environ
@@ -171,9 +172,9 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mohammad963yusuf@gmail.com'
-EMAIL_HOST_PASSWORD = 'chfakwhkiqiokxbl'
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
